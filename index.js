92178d253f4ebb51e8d6d8c7685b590642786083
Ziyoviddin0923 <ziyoviddinmoydinov2309@gmail.com>
2024-03-30 17:57:18 +0500

    TEST PROJECT

---
+++ index.js
@@ -0,0 +1,65 @@
+//  1-misol
+
+// let musbat = +prompt("ixtiyoriy son kiriting");
+// if (musbat > 0 && musbat != 0) {
+//     document.write("musbat");
+// } else if (musbat < 0 && musbat != 0) {
+//     document.write("manfiy");
+// }
+//  2-misol
+
+// let number = +prompt("ixtiyoriy son kiriting");
+// if (number <= 100 && number >= 90) {
+//     document.write("A");
+// }else if  (number <=89 && number >=80){
+//     document.write("B");
+// }else if (number <=79 && number >=70){
+//     document.write("C");
+// }else if (number <=69 && number >=60 ){
+//     document.write ("D");
+// } else if (number <=59 && number >=50){
+//     document.write("F")
+// }
+
+// 3-misol
+
+// let year = +prompt ("yilni kiriting");
+// if ((year%4==0 && year%100 !=0)|| year%400==0){
+//    document.write('kabisa yili');
+//   } else {
+//     document.write("kabisa yili emas!!!");
+//   }
+
+// 4-misol
+
+// let musbat = +prompt ("ixtiyoriy son kiritin");
+// if (musbat > 0 && musbat != 0) {
+//     document.write("musbat");
+// }else if (musbat < 0 && musbat != 0){
+//     document.write("manfiy");
+// }else if (musbat == 0){
+//     document.write ("bu son 0 ga teng");
+// }
+
+// 5-misol
+
+// let number1 = +prompt('1- ixtiyoriy son kiriting');
+// let number2 = +prompt('2- ixtiyoriy son kiriting');
+// if (number1>number2)
+//     document.write ('1-son:', number1);
+//  } else if (number_1 < number_2){
+//      document.write('2-son', number_2);
+//  }
+
+//  6-misol
+
+//  let number_1 = +prompt("1- ixtiyoriy son kiriting");
+//  let number_2 = +prompt("2- ixtiyoriy son kiriting");
+//  let number_3 = +prompt("3- ixtiyoriy son kiriting");
+// if (number_1 < number_2 && number_2 < number_3){
+//     document.write("1-son kichigi:", number_1);
+// }else if (number_1 > number_2 && number_2 < number_3){
+//     document.write("2-son kichigi:", number_2);
+// }else if (number_1 > number_3 && number_3 < number_2){
+//     document.write("3-son kichigi:", number_3);
+// }
